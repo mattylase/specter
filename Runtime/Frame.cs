8ea2cfcd90728dfc9ac2e466729285a5f159ebca
lase <matthew.laser@gmail.com>
2021-10-10 12:38:35 -0400

    Adding base classes for starting a capture

---
+++ Runtime/Frame.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Specter
+{
+    internal class Frame { }
+    internal class DeterministicFrame: Frame { }
+    internal class ImperativeFrame: Frame { }
+}
